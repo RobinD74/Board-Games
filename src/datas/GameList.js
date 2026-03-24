be3bedec6e9758baec14c6b25ad23861dfe07d15
RobinD74 <robin.ducret@orange.fr>
2026-03-24 11:37:02 +0100

    Typo

--- src/datas/GameList.js
+++ src/datas/GameList.js
@@ -55,7 +55,7 @@ export async function fetchExtensions(gameId) {
         .from('extensions_ext')
         .select('*')
         .eq('game_id_ext', gameId)
-        .neq('has_been_deleted', false)
+        .neq('has_been_deleted', true)
         .order('name_ext', { ascending: true });
 
     if (error) {
